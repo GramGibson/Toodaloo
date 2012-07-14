3ba3be948baff0eb66c10cde2f5a796426768758
Gram Gibson <gram.gibson@gmail.com>
2012-07-13 20:33:16 -0400

    Add data access to Todo controller

--- Toodaloo/Controllers/TodoController.cs
+++ Toodaloo/Controllers/TodoController.cs
@@ -4,11 +4,23 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Toodaloo.Infrastructure;
+using Toodaloo.Models;
 
 namespace Toodaloo.Controllers {
 	public class TodoController : ApplicationController {
+		private readonly TodoContext _ctx;
+
+		public TodoController() {
+			_ctx = new TodoContext();
+		}
+
 		public ActionResult Index() {
 			return View();
+		}
+
+		protected override void Dispose(bool disposing) {
+			_ctx.Dispose();
+			base.Dispose(disposing);
 		}
 	}
 }
