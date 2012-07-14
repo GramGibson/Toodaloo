a438156396acd0ee80e09dae3a62f2fe5a55d368
Gram Gibson <gram.gibson@gmail.com>
2012-07-13 20:49:40 -0400

    Add CRUD for Todo controller

--- Toodaloo/Controllers/TodoController.cs
+++ Toodaloo/Controllers/TodoController.cs
@@ -15,7 +15,44 @@ namespace Toodaloo.Controllers {
 		}
 
 		public ActionResult Index() {
-			return View();
+			return View(_ctx.Todos.ToList());
+		}
+
+		[HttpPost]
+		[RequireAjax]
+		public ActionResult Create(Todo todo) {
+			todo.LastModified = DateTime.Now;
+			_ctx.Todos.Add(todo);
+			_ctx.SaveChanges();
+
+			return PartialView("_Created", todo);
+		}
+
+		[RequireAjax]
+		public ActionResult Edit(int id) {
+			var todo = _ctx.Todos.Where(w => w.Id == id).FirstOrDefault();
+
+			return PartialView("_ShowEdit", todo);
+		}
+
+		[HttpPost]
+		[RequireAjax]
+		public ActionResult Edit(int id, Todo todo) {
+			var updated = _ctx.Todos.Where(w => w.Id == id).FirstOrDefault();
+			UpdateModel(updated);
+			updated.LastModified = DateTime.Now;
+			_ctx.SaveChanges();
+
+			return PartialView("_Edited", updated);
+		}
+
+		[HttpPost]
+		[RequireAjax]
+		public ActionResult Delete(int id) {
+			var todo = _ctx.Todos.Where(w => w.Id == id).FirstOrDefault();
+			_ctx.Todos.Remove(todo);
+
+			return PartialView("_Deleted", id);
 		}
 
 		protected override void Dispose(bool disposing) {
