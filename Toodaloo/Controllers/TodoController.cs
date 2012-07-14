f57ad2343e36408df2ecfb6cf184344a190a1810
Gram Gibson <gram.gibson@gmail.com>
2012-07-13 20:19:44 -0400

    Create base controller

--- Toodaloo/Controllers/TodoController.cs
+++ Toodaloo/Controllers/TodoController.cs
@@ -3,9 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Toodaloo.Infrastructure;
 
 namespace Toodaloo.Controllers {
-	public class TodoController : Controller {
+	public class TodoController : ApplicationController {
 		public ActionResult Index() {
 			return View();
 		}
