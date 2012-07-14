c8822d1b3805582225775e75d138e069972046c8
Gram Gibson <gram.gibson@gmail.com>
2012-07-13 20:03:58 -0400

    Initial commit

---
+++ Toodaloo/App_Start/BundleConfig.cs
@@ -0,0 +1,10 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace Toodaloo {
+	public class BundleConfig {
+		public static void RegisterBundles(BundleCollection bundles) {
+			bundles.Add(new StyleBundle("~/css").Include("~/Content/Css/default.css"));
+		}
+	}
+}
