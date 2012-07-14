df0ded7b27b4333f81e3925de8e4126ccc58455a
Gram Gibson <gram.gibson@gmail.com>
2012-07-13 20:42:35 -0400

    Add static Extensions class + EscapeJavascript helper

---
+++ Toodaloo/Helpers/Extensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Toodaloo.Helpers {
+	public static class Extensions {
+		public static HtmlString EscapeJavascript(this HtmlString text) {
+			return new HtmlString(text
+				 .ToHtmlString()
+				 .Replace("\r\n", string.Empty)
+				 .Replace("\n", string.Empty)
+				 .Replace("\r", string.Empty)
+				 .Replace("</", @"<\/")
+				 .Replace("'", "\\'")
+				 .Replace("\"", "\\\""));
+		}
+	}
+}
