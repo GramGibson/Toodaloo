5075dcbcb43b526c8fd78f9833d4468055c71105
Gram Gibson <gram.gibson@gmail.com>
2012-07-13 20:39:57 -0400

    Add RequireAjax attribute

---
+++ Toodaloo/Infrastructure/RequireAjaxAttribute.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Toodaloo.Infrastructure {
+	/// <summary>
+	/// Requires that the current request was made via ajax,
+	/// all other callers will receive a 404
+	/// </summary>
+	public class RequireAjaxAttribute : ActionFilterAttribute {
+		public override void OnActionExecuting(ActionExecutingContext filterContext) {
+			if (!filterContext.HttpContext.Request.IsAjaxRequest())
+				filterContext.Result = new HttpStatusCodeResult(404);
+
+			base.OnActionExecuting(filterContext);
+		}
+	}
+}
