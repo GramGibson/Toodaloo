26c71ad5fe5afb54ee5c0807c1bdef9e60547609
Gram Gibson <gram.gibson@gmail.com>
2012-07-13 20:17:58 -0400

    Create Home controller that redirects to Todo list + initial Todo controller

---
+++ Toodaloo/Controllers/HomeController.cs
@@ -0,0 +1,9 @@
+﻿using System.Web.Mvc;
+
+namespace Toodaloo.Controllers {
+	public class HomeController : Controller {
+		public ActionResult Index() {
+			return RedirectToAction("", "todo");
+		}
+	}
+}
